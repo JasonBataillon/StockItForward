c85a4734b50cbafb9dd427f64f76899bd9cab375
Per-Caelum <mzeraphina@gmail.com>
2025-02-11 11:32:52 -0600

    requested by aaron

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 //import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
+
+//for aaron and jason
 import "./App.css";
 import Register from "./components/register";
 import Login from "./components/login";
