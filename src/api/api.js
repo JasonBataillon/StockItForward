75c6f2441acae177bfcccb3521c7b68f8eee2037
Your Name <jasonbataillon@gmail.com>
2025-02-11 12:54:47 -0500

    Fixed connection errors

--- src/api/api.js
+++ src/api/api.js
@@ -1,21 +1,21 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 // const API_URL = `https://stockitback.onrender.com`;
-const API_URL = `https://localhost:3000`;
+const API_URL = `https://stockitback.onrender.com`;
 
 const api = createApi({
-  reducerPath: "api",
+  reducerPath: 'api',
   baseQuery: fetchBaseQuery({
     baseUrl: API_URL,
     prepareHeaders: (headers) => {
-      const token = localStorage.getItem("token");
+      const token = localStorage.getItem('token');
       if (token) {
-        headers.set("authorization", `Bearer ${token}`);
+        headers.set('authorization', `Bearer ${token}`);
       }
       return headers;
     },
   }),
-  tagTypes: ["User"],
+  tagTypes: ['User'],
   endpoints: () => ({}),
 });
 export default api;
