d24f36edefbf6355ffa6d54cde898f4fe5cb02e3
Per-Caelum <mzeraphina@gmail.com>
2025-02-21 03:30:20 -0600

    pre-main-checkout test. adding useStates for formData and formData

--- src/components/stockChart.jsx
+++ src/components/stockChart.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   LineChart,
   Line,
@@ -7,7 +7,7 @@ import {
   XAxis,
   YAxis,
   Tooltip,
-} from 'recharts';
+} from "recharts";
 
 const StockChart = ({ onStockPriceChange }) => {
   const location = useLocation();
@@ -20,77 +20,95 @@ const StockChart = ({ onStockPriceChange }) => {
 
   useEffect(() => {
     if (!stockTicker) {
-      console.error('No stockTicker found in location state');
-      navigate('/stockSearch'); // Redirect to stock search if no stockTicker is found
+      console.error("No stockTicker found in location state");
+      navigate("/stockSearch"); // Redirect to stock search if no stockTicker is found
     }
   }, [stockTicker, navigate]);
+
+  useEffect(() => {
+    if (stockTicker) {
+      fetchStockData();
+    }
+  }, [
+    stockTicker,
+    API_KEY,
+    queryTimespan,
+    queryFromDate,
+    queryToDate,
+    queryAdjusted,
+  ]);
 
   const [data, setData] = useState([]); //declaring hook for data storage
   const [loading, setLoading] = useState(true); //declaring hook to indicate whether app is working instead of blank screening
   const [stockPrice, setStockPrice] = useState(initialStockPrice);
   const [marketCap, setMarketCap] = useState(null);
-  const [stockName, setStockName] = useState(initialStockName);
+  const [stockName, setStockName] = useState(initialStockName); //at no point is setStockName used. I dont know why this is the case.
+
+  const [watchlistMessage, setWatchlistMessage] = useState(null);
+  const [queryTimespan, setQueryTimespan] = useState("day");
+  const [queryFromDate, setQueryFromDate] = useState("2023-01-01");
+  const [queryToDate, setQueryToDate] = useState("2023-12-31");
+  const [queryAdjusted, setQueryAdjusted] = useState(true);
+  const [queryTrigger, setQueryTrigger] = useState(0);
+
   const API_KEY = import.meta.env.VITE_POLYGON_API_KEY;
 
-  useEffect(() => {
-    const fetchStockData = async () => {
-      try {
-        const multiplier = 1; // Change this to get different time scale
-        const timespan = 'day'; // day, week, month, quarter, year
-        const from = '2023-01-01'; // starting YEAR-MO-DA
-        const to = '2023-12-31'; // ending YEAR-MO-DA
-        const response = await fetch(
-          `https://api.polygon.io/v2/aggs/ticker/${stockTicker}/range/${multiplier}/${timespan}/${from}/${to}?adjusted=true&sort=asc&limit=30&apiKey=${API_KEY}`
-        );
+  const fetchStockData = async () => {
+    try {
+      const multiplier = 1; // Change this to get different time scale
+      const timespan = "day"; // day, week, month, quarter, year
+      const from = "2023-01-01"; // starting YEAR-MO-DA
+      const to = "2023-12-31"; // ending YEAR-MO-DA
+      const response = await fetch(
+        `https://api.polygon.io/v2/aggs/ticker/${stockTicker}/range/${multiplier}/${timespan}/${from}/${to}?adjusted=true&sort=asc&limit=30&apiKey=${API_KEY}`
+      );
 
-        if (!response.ok) {
-          if (response.status === 401) {
-            throw new Error('Unauthorized: Check your API key');
-          } else {
-            throw new Error('Network response was not ok');
-          }
+      if (!response.ok) {
+        if (response.status === 401) {
+          throw new Error("Unauthorized: Check your API key");
+        } else {
+          throw new Error("Network response was not ok");
         }
-
-        const json = await response.json();
-
-        if (json.results) {
-          const formattedData = json.results.map((item) => ({
-            date: new Date(item.t).toLocaleDateString(),
-            close: item.c,
-          }));
-
-          setData(formattedData);
-          setStockPrice(json.results[0].c);
-          setMarketCap(json.results[0].marketCap || 0);
-
-          if (onStockPriceChange) {
-            onStockPriceChange(json.results[0].c);
-          }
-        }
-      } catch (error) {
-        console.error('Error fetching stock data:', error);
-      } finally {
-        setLoading(false);
-        setLoading(false);
       }
-    };
+
+      const json = await response.json();
+
+      if (json.results) {
+        const formattedData = json.results.map((item) => ({
+          date: new Date(item.t).toLocaleDateString(),
+          close: item.c,
+        }));
+
+        setData(formattedData);
+        setStockPrice(json.results[0].c);
+        setMarketCap(json.results[0].marketCap || 0);
+
+        if (onStockPriceChange) {
+          onStockPriceChange(json.results[0].c);
+        }
+      }
+    } catch (error) {
+      console.error("Error fetching stock data:", error);
+    } finally {
+      setLoading(false);
+    }
 
     if (stockTicker) {
       fetchStockData();
     }
-  }, [API_KEY, stockTicker, onStockPriceChange]);
+  };
 
   const saveStockToWatchlist = async () => {
     try {
-      const token = localStorage.getItem('token');
+      const token = localStorage.getItem("token");
       if (!token) {
-        throw new Error('No token found');
+        throw new Error("No token found");
       }
 
-      const response = await fetch('http://localhost:3000/watchlist/add', {
-        method: 'POST',
+      const response = await fetch("http://localhost:3000/watchlist/add", {
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
+          "Content-Type": "application/json",
           Authorization: `Bearer ${token}`,
         },
         body: JSON.stringify({
@@ -102,16 +120,19 @@ const StockChart = ({ onStockPriceChange }) => {
       });
 
       const result = await response.json();
-      console.log('Stock saved to watchlist:', result);
+      console.log("Stock saved to watchlist:", result);
 
       if (response.ok) {
-        alert('This stock has been added into your watchlist!', result);
+        setWatchlistMessage(
+          "This stock has been added into your watchlist!",
+          result
+        );
       } else {
-        throw new Error('Failed to save stock to watchlist');
+        throw new Error("Failed to save stock to watchlist");
       }
     } catch (error) {
-      alert('This stock is already in your watchlist!');
-      console.error('Error saving stock to watchlist:', error);
+      setWatchlistMessage(error, "Could it already be in your watchlist?");
+      console.error("Error saving stock to watchlist:", error);
     }
   };
 
@@ -119,14 +140,33 @@ const StockChart = ({ onStockPriceChange }) => {
     return <div>Loading...</div>;
   }
 
+  useEffect(() => {
+    if (stockTicker) {
+      fetchStockData();
+    }
+  }, [
+    stockTicker,
+    API_KEY,
+    queryTimespan,
+    queryFromDate,
+    queryToDate,
+    queryAdjusted,
+    queryTrigger,
+  ]);
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setQueryTrigger((prev) => prev + 1);
+  };
+
   return (
     <div>
       <h1>Stock Charts</h1>
       <div>{stockTicker}</div>
       <div>{stockName}</div>
       <p>
-        Current Price:{' '}
-        {stockPrice !== null ? `$${stockPrice.toFixed(2)}` : 'Loading...'}
+        Current Price:{" "}
+        {stockPrice !== null ? `$${stockPrice.toFixed(2)}` : "Loading..."}
       </p>
       <LineChart
         width={800}
@@ -137,10 +177,50 @@ const StockChart = ({ onStockPriceChange }) => {
         <Line type="monotone" dataKey="close" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" />
         <XAxis dataKey="date" />
-        <YAxis domain={['auto', 'dataMax + 5']} />
+        <YAxis domain={["auto", "dataMax + 5"]} />
         <Tooltip />
       </LineChart>
       <button onClick={saveStockToWatchlist}>Save to Watchlist</button>
+      <div>{watchlistMessage}</div>
+      <form onSubmit={handleFormSubmit}>
+        <label>Timespan:</label>
+        <select
+          id="timespan"
+          name="timespan"
+          onChange={(e) => setQueryTimespan(e.target.value)}
+        >
+          <option value="day">Day</option>
+          <option value="week">Week</option>
+          <option value="month">Month</option>
+          <option value="quarter">Quarter</option>
+          <option value="year">Year</option>
+        </select>
+        <br />
+        <label htmlFor="date">From:</label>{" "}
+        <input
+          type="date"
+          id="fromDate"
+          name="date"
+          onChange={(e) => setQueryFromDate(e.target.value)}
+        />
+        <br />
+        <label htmlFor="date">To:</label>
+        <input
+          type="date"
+          id="toDate"
+          name="date"
+          onChange={(e) => setQueryToDate(e.target.value)}
+        />
+        <br />
+        <label>Adjusted True:</label>
+        <input
+          type="checkbox"
+          id="boolAdjusted"
+          name="boolAdjusted"
+          onChange={(e) => setQueryAdjusted(e.target.checked)}
+        />
+        <br />
+      </form>
     </div>
   );
 };
