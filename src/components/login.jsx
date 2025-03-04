fcd5eb95c83bab62e68e99500c541d3456df09ce
Sarah Parent <sarahparent@Sarahs-MacBook-Air.local>
2025-03-04 08:49:58 -0600

    fixed link

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -11,19 +11,16 @@ const Login = () => {
     // Here you would typically handle the login logic, such as sending the data to your server
     try {
       // const response = await fetch('https://stockitback.onrender.com/login', {
-      const response = await fetch(
-        "https://stockitforward-eeb4.onrender.com/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username,
-            password,
-          }),
-        }
-      );
+      const response = await fetch("http://localhost:3000/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
       if (!response.ok) {
         throw new Error("Login failed");
       }
